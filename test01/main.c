586b2bfd8c9c86d4228d9d1e0dd3397b9dbe4762
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-08-06 08:05:18 +0700

    createVariableC

--- test01/main.c
+++ test01/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main()
 {
-	int a, b;
+	int a, b,c;
 
 	printf("This is sum program with 2 number\nEnter your first number : ");
 	scanf_s("%d",&a);
 	printf("Enter your second number :");
 	scanf_s("%d", &b);
-	printf("%d + %d = %d",a,b,a+b);
+	c = a + b;
+	printf("%d + %d = %d",a,b,c);
 	
 }
